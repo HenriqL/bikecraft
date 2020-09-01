a18f9049a04004265612983e8b7f0ee685c3b74a
Henrique Lopes <henriquelopesls032@gmail.com>
2020-09-01 16:13:39 -0300

    final update

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+new SimpleSlide({
+    slide: 'coments', // nome do atributo data-slide="principal"
+    time: 5000, // tempo de transição dos slides
+
+  });
+new SimpleSlide({
+    slide: 'portfolio', // nome do atributo data-slide="principal"
+    time: 5000,
+    nav: true, // tempo de transição dos slides
+
+  });
+
+
+  new SimpleAnime();
